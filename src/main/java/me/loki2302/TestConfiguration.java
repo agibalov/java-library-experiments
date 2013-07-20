9f3049ddbac8d757ecafa8a224651a1ea39d3a34
loki2302 <loki2302@loki2302.me>
2013-07-20 19:45:48 +0400

    Test is now designed as test

---
+++ src/main/java/me/loki2302/TestConfiguration.java
@@ -0,0 +1,5 @@
+package me.loki2302;
+
+public abstract class TestConfiguration {
+    public static final String RabbitMQHostName = "win7dev-home";
+}
--- src/test/java/me/loki2302/AppTest.java
+++ src/test/java/me/loki2302/AppTest.java
@@ -1,38 +1,57 @@
 package me.loki2302;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import java.io.IOException;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import org.junit.Test;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.ConsumerCancelledException;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.QueueingConsumer.Delivery;
+import com.rabbitmq.client.ShutdownSignalException;
+
+public class AppTest {
+    @Test
+    public void test() throws IOException, ShutdownSignalException, ConsumerCancelledException, InterruptedException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost(TestConfiguration.RabbitMQHostName);
+        
+        boolean receivedMessage = false;
+        
+        Connection connection = null;
+        try {
+            connection = connectionFactory.newConnection();
+            
+            Channel channel = null;
+            try {
+                channel = connection.createChannel();
+                channel.queueDeclare("HelloQueue", false, false, false, null);
+                channel.basicPublish("", "HelloQueue", null, "hello there!".getBytes());
+                
+                QueueingConsumer consumer = new QueueingConsumer(channel);
+                channel.basicConsume("HelloQueue", true, consumer);
+                
+                Delivery delivery = consumer.nextDelivery();
+                String m = new String(delivery.getBody());
+                assertEquals("hello there!", m);
+                receivedMessage = true;
+            } finally {
+                if(channel != null) {
+                    channel.close();
+                }
+            }
+        } finally {
+            if(connection != null) {
+                connection.close();
+            }
+        }
+        
+        if(!receivedMessage) {
+            fail();
+        }
+    }    
 }
