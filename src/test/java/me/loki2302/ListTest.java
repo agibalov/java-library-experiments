4d969da57bdfb19d867df951589d228c5201d589
loki2302 <loki2302@loki2302.me>
2013-10-13 17:33:39 +0400

    Add list test

--- src/test/java/me/loki2302/ListTest.java
+++ src/test/java/me/loki2302/ListTest.java
@@ -1,4 +1,26 @@
 package me.loki2302;
 
-public class ListTest extends AbstractJedisTest {        
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class ListTest extends AbstractJedisTest {
+    @Test
+    public void canPushRightItems() {
+        jedis.rpush("x", "x1", "x2");
+        List<String> items = jedis.lrange("x", 0, -1);
+        assertEquals(2, items.size());
+        assertEquals("x1", items.get(0));
+        assertEquals("x2", items.get(1));
+    }
+    
+    @Test
+    public void canPopRightItems() {
+        jedis.rpush("x", "x1", "x2");
+        String x = jedis.rpop("x");
+        assertEquals("x2", x);
+        assertEquals(1, (long)jedis.llen("x"));
+    }
 }
