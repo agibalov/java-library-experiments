593ed6550aa979c447eadba98433e67ba81132d2
loki2302 <loki2302@loki2302.me>
2013-11-14 23:53:34 +0400

    Initial version

---
+++ src/main/java/me/loki2302/CalculatorApiImpl.java
@@ -0,0 +1,8 @@
+package me.loki2302;
+
+public class CalculatorApiImpl implements CalculatorApi {
+    @Override
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }        
+}
---
+++ src/main/java/me/loki2302/MyApplication.java
@@ -0,0 +1,17 @@
+package me.loki2302;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class MyApplication extends Application {
+    @Override
+    public Set<Object> getSingletons() {
+        Set<Object> services = new HashSet<Object>();
+        services.add(new CalculatorApiImpl());
+        return services;
+    }        
+}
