a237ac5ab49984d225ce514eeb4d0f77c2e27fc5
loki2302 <loki2302@loki2302.me>
2014-05-13 12:48:49 -0700

    Added collection test

--- src/test/java/me/loki2302/DummyTest.java
+++ src/test/java/me/loki2302/DummyTest.java
@@ -6,18 +6,34 @@ import org.thymeleaf.context.Context;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.TemplateResolver;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertTrue;
 
 public class DummyTest {
     @Test
-    public void dummy() {
+    public void canInsertText() {
         Map<String, String> variables = new HashMap<String, String>();
         variables.put("message", "hello");
-        String xml = render("1", variables);
+        String xml = render("simple", variables);
         assertTrue(xml.contains("<name>hello</name>"));
+    }
+
+    @Test
+    public void canInsertCollection() {
+        List<Person> people = Arrays.asList(
+                new Person(111, "john"),
+                new Person(222, "jane")
+        );
+
+        Map<String, Object> variables = new HashMap<String, Object>();
+        variables.put("people", people);
+        String xml = render("people", variables);
+        assertTrue(xml.contains("<person id=\"111\">john</person>"));
+        assertTrue(xml.contains("<person id=\"222\">jane</person>"));
     }
 
     private String render(String templateName, Map<String, ?> variables) {
@@ -35,5 +51,15 @@ public class DummyTest {
         String result = templateEngine.process(templateName, context);
 
         return result;
+    }
+
+    public static class Person {
+        public int id;
+        public String name;
+
+        public Person(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
     }
 }
