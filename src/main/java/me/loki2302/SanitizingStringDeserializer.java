b7e261a43c945dc303bb532dadbc352990f5ffb0
loki2302 <loki2302@loki2302.me>
2014-06-03 12:19:39 -0700

    Added sanitizing deserializer: demonstrates how to preprocess the REST API request even before it gets validated

---
+++ src/main/java/me/loki2302/SanitizingStringDeserializer.java
@@ -0,0 +1,53 @@
+package me.loki2302;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.BeanProperty;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.deser.ContextualDeserializer;
+
+import java.io.IOException;
+
+public class SanitizingStringDeserializer extends JsonDeserializer<String> implements ContextualDeserializer {
+    @Override
+    public JsonDeserializer<?> createContextual(
+            DeserializationContext ctxt,
+            BeanProperty property) throws JsonMappingException {
+
+        Trim trimAnnotation = property.getAnnotation(Trim.class);
+        boolean shouldTrim = trimAnnotation != null;
+
+        return new SanitizationAwareStringDeserializer(shouldTrim);
+    }
+
+    @Override
+    public String deserialize(
+            JsonParser jp,
+            DeserializationContext ctxt) throws IOException, JsonProcessingException {
+
+        throw new RuntimeException();
+    }
+
+    private static class SanitizationAwareStringDeserializer extends JsonDeserializer<String> {
+        private final boolean shouldTrim;
+
+        public SanitizationAwareStringDeserializer(boolean shouldTrim) {
+            this.shouldTrim = shouldTrim;
+        }
+
+        @Override
+        public String deserialize(
+                JsonParser jp,
+                DeserializationContext ctxt) throws IOException, JsonProcessingException {
+
+            String value = jp.getValueAsString();
+            if(value != null && shouldTrim) {
+                value = value.trim();
+            }
+
+            return value;
+        }
+    }
+}
--- src/test/java/me/loki2302/HasIntValueTest.java
+++ src/test/java/me/loki2302/HasIntValueTest.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
-public class AppTest {
+public class HasIntValueTest {
     private ObjectMapper objectMapper;
 
     @Before
