c6805a250ee63fba5b99909a88ff66e692220b30
loki2302 <loki2302@loki2302.me>
2014-09-06 15:16:44 +0400

    Added ServletContextHandler test

--- src/test/java/me/loki2302/AppTest.java
+++ src/test/java/me/loki2302/AppTest.java
@@ -1,14 +1,5 @@
 package me.loki2302;
 
-import static org.junit.Assert.*;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -18,11 +9,51 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.junit.Test;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class AppTest {
     @Test
-    public void dummyTest() throws Exception {
+    public void testServletContextHandler() throws Exception {
+        ServletContextHandler servletContextHandler = new ServletContextHandler();
+        servletContextHandler.setContextPath("/");
+        servletContextHandler.addServlet(new ServletHolder(new HttpServlet() {
+            @Override
+            public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+                PrintWriter printWriter = res.getWriter();
+                printWriter.println("hello");
+                printWriter.close();
+            }
+        }), "/*");
+
+        Server server = new Server(8080);
+        server.setHandler(servletContextHandler);
+        server.start();
+
+        try {
+            String responseString = getAsString("http://localhost:8080/");
+            assertEquals("hello\n", responseString);
+        } finally {
+            server.stop();
+        }
+    }
+
+    @Test
+    public void testCustomHandler() throws Exception {
         Server server = new Server(8080);
         server.setHandler(new AbstractHandler() {
             @Override
@@ -36,33 +67,34 @@ public class AppTest {
                 response.setStatus(HttpServletResponse.SC_OK);
                 response.getWriter().println("hello there");
                 ((Request)request).setHandled(true);
-                
-                System.out.printf("request from %s, URL=%s\n", request.getRemoteAddr(), request.getRequestURI());                
             }
         });
         server.start();
                 
         try {
-            CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-            try {
-                HttpGet httpGet = new HttpGet("http://localhost:8080/");
-                HttpResponse httpResponse = httpClient.execute(httpGet);
-                assertEquals(200, httpResponse.getStatusLine().getStatusCode());
-                
-                HttpEntity responseEntity = httpResponse.getEntity();
-                assertNotNull(responseEntity);
-                InputStream inputStream = responseEntity.getContent();
-                try {
-                    String responseString = IOUtils.toString(inputStream);
-                    assertEquals("hello there\n", responseString);
-                } finally {
-                    inputStream.close();
-                }
-            } finally {
-                httpClient.close();
-            }
+            String responseString = getAsString("http://localhost:8080/");
+            assertEquals("hello there\n", responseString);
         } finally {
             server.stop();            
         }       
+    }
+
+    private static String getAsString(String url) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        try {
+            HttpGet httpGet = new HttpGet("http://localhost:8080/");
+            HttpResponse httpResponse = httpClient.execute(httpGet);
+            HttpEntity responseEntity = httpResponse.getEntity();
+            assertNotNull(responseEntity);
+            InputStream inputStream = responseEntity.getContent();
+            try {
+                String responseString = IOUtils.toString(inputStream);
+                return responseString;
+            } finally {
+                inputStream.close();
+            }
+        } finally {
+            httpClient.close();
+        }
     }
 }
