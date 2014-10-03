a65a3b755d35a759958528fe0f0c01daee706022
loki2302 <loki2302@loki2302.me>
2014-10-03 11:21:54 +0400

    Added ReduceTest

--- src/main/java/me/loki2302/App.java
+++ src/main/java/me/loki2302/App.java
@@ -2,39 +2,51 @@ package me.loki2302;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.SerializationConfig;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        PersonEntity personEntity = new PersonEntity();
-        personEntity.id = 123L;
-        personEntity.name = "loki2302";
+        PostEntity postEntity1 = new PostEntity();
+        postEntity1.id = 111L;
+        postEntity1.text = "hello";
+
+        PostEntity postEntity2 = new PostEntity();
+        postEntity2.id = 222L;
+        postEntity2.text = "there";
+
+        BlogEntity blogEntity = new BlogEntity();
+        blogEntity.id = 123L;
+        blogEntity.name = "loki2302";
+        blogEntity.posts = Arrays.asList(postEntity1, postEntity2);
 
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setFieldMatchingEnabled(true);
-        /*modelMapper.addMappings(new PropertyMap<PersonEntity, PersonDTOWithLongId>() {
+        modelMapper.addConverter(new Converter<List, Integer>() {
             @Override
-            protected void configure() {
-                map(source.id, destination.id);
-                map(source.name, destination.name);
+            public Integer convert(MappingContext<List, Integer> context) {
+                return context.getSource().size();
             }
-        });*/
+        });
 
-        PersonDTOWithLongId personDTOWithLongId = modelMapper.map(personEntity, PersonDTOWithLongId.class);
-        System.out.println(personDTOWithLongId);
-
-        PersonDTOWithIntId personDTOWithIntId = modelMapper.map(personEntity, PersonDTOWithIntId.class);
-        System.out.println(personDTOWithIntId);
-
-        PersonDTOWithStringId personDTOWithStringId = modelMapper.map(personEntity, PersonDTOWithStringId.class);
-        System.out.println(personDTOWithStringId);
+        BriefBlogDTO briefBlogDTO = modelMapper.map(blogEntity, BriefBlogDTO.class);
+        System.out.println(briefBlogDTO);
     }
 
-    public static class PersonEntity {
+    public static class BlogEntity {
         public Long id;
         public String name;
+        public List<PostEntity> posts;
+    }
+
+    public static class PostEntity {
+        public Long id;
+        public String text;
     }
 
     public static abstract class FancyToString {
@@ -50,18 +62,9 @@ public class App {
         }
     }
 
-    public static class PersonDTOWithLongId extends FancyToString {
-        public Long id;
+    public static class BriefBlogDTO extends FancyToString {
+        public long id;
         public String name;
-    }
-
-    public static class PersonDTOWithIntId extends FancyToString {
-        public int id;
-        public String name;
-    }
-
-    public static class PersonDTOWithStringId extends FancyToString {
-        public String id;
-        public String name;
+        public Integer posts; // can't make Converter<xxx, int>
     }
 }
