f04331ae03093594c00d2755f85b008db9342574
loki2302 <loki2302@loki2302.me>
2014-10-09 09:11:23 +0400

    Added canComputeDistanceBetweenLineAndAPoint test for JavaGeom

--- src/test/java/me/loki2302/JavaGeomTests.java
+++ src/test/java/me/loki2302/JavaGeomTests.java
@@ -1,6 +1,7 @@
 package me.loki2302;
 
 import math.geom2d.Point2D;
+import math.geom2d.line.StraightLine2D;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -13,5 +14,15 @@ public class JavaGeomTests {
         Point2D p1 = new Point2D(-1, 0);
         Point2D p2 = new Point2D(1, 0);
         assertEquals(2, p1.distance(p2), Tolerance);
+    }
+
+    @Test
+    public void canComputeDistanceBetweenLineAndAPoint() {
+        Point2D p = new Point2D(0, 1);
+        StraightLine2D line = new StraightLine2D(
+                new Point2D(0, 0),
+                new Point2D(1, 0));
+
+        assertEquals(1, line.distance(p), Tolerance);
     }
 }
