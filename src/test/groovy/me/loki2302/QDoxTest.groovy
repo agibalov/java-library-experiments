d29519c831cc897d5aa4bcf3d5ca4091be37670d
loki2302 <loki2302@loki2302.me>
2014-11-25 10:07:44 +0300

    Added more assertions

--- src/test/groovy/me/loki2302/QDoxTest.groovy
+++ src/test/groovy/me/loki2302/QDoxTest.groovy
@@ -2,10 +2,11 @@ package me.loki2302;
 
 import com.thoughtworks.qdox.JavaProjectBuilder
 import com.thoughtworks.qdox.model.JavaClass
+import com.thoughtworks.qdox.model.JavaMethod
 import com.thoughtworks.qdox.model.JavaSource
 import org.junit.Test
 
-import static org.junit.Assert.assertEquals
+import static org.junit.Assert.*
 
 class QDoxTest {
     @Test
@@ -15,14 +16,27 @@ class QDoxTest {
 package me.loki2302;
 
 public class Dummy {
+    public int addNumbers(float a, double b) {
+        return a + b;
+    }
 }
 '''))
 
         List<JavaClass> classes = javaSource.classes
         assertEquals(1, classes.size())
 
-        JavaClass dummyClass = classes[0]
+        JavaClass dummyClass = classes.first()
         assertEquals('Dummy', dummyClass.name)
+        assertTrue(dummyClass.public)
         assertEquals('me.loki2302', dummyClass.packageName)
+
+        assertEquals(1, dummyClass.methods.size())
+        JavaMethod addNumbersMethod = dummyClass.methods.first()
+        assertEquals('addNumbers', addNumbersMethod.name)
+        assertTrue(addNumbersMethod.public)
+
+        assertEquals('int', addNumbersMethod.returns.value)
+        assertEquals('float', addNumbersMethod.parameters[0].type.value)
+        assertEquals('double', addNumbersMethod.parameters[1].type.value)
     }
 }
