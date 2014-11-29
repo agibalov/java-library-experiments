474845b3b7e859437aa749cb643c3c036d00af9d
loki2302 <loki2302@loki2302.me>
2014-11-29 13:38:17 +0300

    Added a Java migration test

--- src/test/groovy/me/loki2302/FlywayTest.groovy
+++ src/test/groovy/me/loki2302/FlywayTest.groovy
@@ -3,19 +3,23 @@ package me.loki2302
 import groovy.sql.Sql
 import org.flywaydb.core.Flyway
 import org.hsqldb.jdbc.JDBCDataSource
+import org.junit.Before
 import org.junit.Test
 
 import javax.sql.DataSource
 import static org.junit.Assert.*
 
 class FlywayTest {
+    private DataSource dataSource
+
+    @Before
+    void constructDataSource() {
+        def databaseName = UUID.randomUUID().toString()
+        dataSource = new JDBCDataSource(url: "jdbc:hsqldb:mem:${databaseName}")
+    }
+
     @Test
-    void dummy() {
-        DataSource dataSource = new JDBCDataSource(url: 'jdbc:hsqldb:mem:testdb')
-
-        def sql = Sql.newInstance(dataSource)
-
-        // v1 - initial version: Posts::title is nullable
+    void canCreateV1Database() {
         def flyway = new Flyway()
         flyway.setDataSource(dataSource)
         flyway.setTarget('1')
@@ -26,20 +30,76 @@ class FlywayTest {
         assertEquals('1', appliedMigrations.first().version.version)
         assertEquals('Initial version', appliedMigrations.first().description)
 
-        sql.execute "insert into Posts(title) values('post one')"
-        sql.execute 'insert into Posts(title) values(null)'
-        assertEquals(2, sql.rows('select * from Posts').size())
-        assertEquals(1, sql.rows('select * from Posts where title is null').size())
+        Sql sql = Sql.newInstance(dataSource)
+        assertEquals(0, sql.firstRow('select count(*) as c from Posts').c)
 
-        // v2: Posts::title is not nullable
+        sql.execute('insert into Posts(title) values(null)')
+        assertEquals(1, sql.firstRow('select count(*) as c from Posts').c)
+        assertEquals(1, sql.firstRow('select count(*) as c from Posts where title is null').c)
+    }
+
+    @Test
+    void canCreateV2Database() {
+        def flyway = new Flyway()
+        flyway.setDataSource(dataSource)
         flyway.setTarget('2')
         flyway.migrate()
-        appliedMigrations = flyway.info().applied()
+
+        def appliedMigrations = flyway.info().applied()
         assertEquals(2, appliedMigrations.size())
+        assertEquals('1', appliedMigrations.first().version.version)
+        assertEquals('Initial version', appliedMigrations.first().description)
         assertEquals('2', appliedMigrations.last().version.version)
         assertEquals('Add not null constraint to post title', appliedMigrations.last().description)
 
-        assertEquals(2, sql.rows('select * from Posts').size())
-        assertEquals(0, sql.rows('select * from Posts where title is null').size())
+        Sql sql = Sql.newInstance(dataSource)
+        assertEquals(0, sql.firstRow('select count(*) as c from Posts').c)
+
+        sql.execute("insert into Posts(title) values('')")
+        assertEquals(1, sql.firstRow('select count(*) as c from Posts').c)
+        assertEquals(0, sql.firstRow('select count(*) as c from Posts where title is null').c)
+    }
+
+    @Test
+    void canUpdateV1DatabaseToV2() {
+        def flyway = new Flyway()
+        flyway.setDataSource(dataSource)
+        flyway.setTarget('1')
+        flyway.migrate()
+
+        Sql sql = Sql.newInstance(dataSource)
+
+        sql.execute('insert into Posts(title) values(null)')
+
+        flyway.setTarget('2')
+        flyway.migrate()
+
+        def appliedMigrations = flyway.info().applied()
+        assertEquals(2, appliedMigrations.size())
+        assertEquals('1', appliedMigrations.first().version.version)
+        assertEquals('Initial version', appliedMigrations.first().description)
+        assertEquals('2', appliedMigrations.last().version.version)
+        assertEquals('Add not null constraint to post title', appliedMigrations.last().description)
+
+        assertEquals(1, sql.firstRow('select count(*) as c from Posts').c)
+        assertEquals(0, sql.firstRow('select count(*) as c from Posts where title is null').c)
+    }
+
+    @Test
+    void canUseJavaMigration() {
+        def flyway = new Flyway()
+        flyway.setDataSource(dataSource)
+        flyway.setTarget('2')
+        flyway.migrate()
+
+        Sql sql = Sql.newInstance(dataSource)
+        sql.execute("insert into Posts(title) values('')")
+
+        flyway.setTarget('3')
+        flyway.migrate()
+
+        def row = sql.firstRow('select * from Posts')
+        assertEquals(0, row.id)
+        assertEquals('post-0', row.title)
     }
 }
