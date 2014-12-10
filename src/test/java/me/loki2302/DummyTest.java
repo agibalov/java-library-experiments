9ea58335ac9040909de4eb3518c52a5fc9817c9d
loki2302 <loki2302@loki2302.me>
2014-12-10 09:23:37 +0300

    Initial version

---
+++ src/test/java/me/loki2302/DummyTest.java
@@ -0,0 +1,73 @@
+package me.loki2302;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Consume;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.guice.CamelModuleWithRouteTypes;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DummyTest {
+    @Test
+    public void canProduceWithInjectedProducerTemplate() {
+        Injector injector = Guice.createInjector(
+                new CamelModuleWithRouteTypes(),
+                new AbstractModule() {
+                    @Override
+                    protected void configure() {
+                        bind(DummyProducer.class).asEagerSingleton();
+                        bind(DummyConsumer.class).asEagerSingleton();
+                    }
+                });
+
+        DummyProducer dummyProducer = injector.getInstance(DummyProducer.class);
+        dummyProducer.produce("hi there");
+
+        DummyConsumer dummyConsumer = injector.getInstance(DummyConsumer.class);
+        assertEquals(1, dummyConsumer.consumptionCount);
+    }
+
+    @Test
+    public void canProduceWithProducerTemplateRetrievedManually() {
+        Injector injector = Guice.createInjector(
+                new CamelModuleWithRouteTypes(),
+                new AbstractModule() {
+                    @Override
+                    protected void configure() {
+                        bind(DummyConsumer.class).asEagerSingleton();
+                    }
+                });
+
+        CamelContext camelContext = injector.getInstance(CamelContext.class);
+
+        ProducerTemplate producerTemplate = camelContext.createProducerTemplate();
+        producerTemplate.setDefaultEndpointUri("direct:test");
+        producerTemplate.sendBody("hi there");
+
+        DummyConsumer dummyConsumer = injector.getInstance(DummyConsumer.class);
+        assertEquals(1, dummyConsumer.consumptionCount);
+    }
+
+    public static class DummyProducer {
+        @Produce(uri = "direct:test")
+        private ProducerTemplate producerTemplate;
+
+        public void produce(String data) {
+            producerTemplate.sendBody(data);
+        }
+    }
+
+    public static class DummyConsumer {
+        public int consumptionCount;
+
+        @Consume(uri = "direct:test")
+        public void consume(String data) {
+            ++consumptionCount;
+        }
+    }
+}
