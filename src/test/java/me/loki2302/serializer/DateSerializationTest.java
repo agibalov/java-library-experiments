18a9d766c432ed075edbafe14dcd1473566a2f3a
loki2302 <loki2302@loki2302.me>
2014-12-16 22:00:56 +0300

    Added ISO8601 serialization test

--- src/test/java/me/loki2302/serializer/DateSerializationTest.java
+++ src/test/java/me/loki2302/serializer/DateSerializationTest.java
@@ -1,8 +1,12 @@
 package me.loki2302.serializer;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.joda.time.format.ISODateTimeFormat;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -25,8 +29,26 @@ public class DateSerializationTest {
         ObjectMapper objectMapper = new ObjectMapper();
         String json = objectMapper.writeValueAsString(post);
 
-        PostWithLongDate postWithStringDate = objectMapper.readValue(json, PostWithLongDate.class);
-        assertEquals(TEST_DATETIME.getMillis(), postWithStringDate.createdAt);
+        PostWithLongDate postWithLongDate = objectMapper.readValue(json, PostWithLongDate.class);
+        assertEquals(TEST_DATETIME.getMillis(), postWithLongDate.createdAt);
+
+        post = objectMapper.readValue(json, Post.class);
+        assertEquals(TEST_DATETIME, new DateTime(
+                post.createdAt,
+                DateTimeZone.forTimeZone(TimeZone.getTimeZone("UTC"))));
+    }
+
+    @Test
+    public void canSerializeAndDeserializeDateAsISO8601String() throws IOException {
+        Post post = new Post();
+        post.createdAt = TEST_DATETIME.toDate();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        String json = objectMapper.writeValueAsString(post);
+
+        PostWithStringDate postWithStringDate = objectMapper.readValue(json, PostWithStringDate.class);
+        assertEquals("2014-12-16T21:36:13.000+0000", postWithStringDate.createdAt);
 
         post = objectMapper.readValue(json, Post.class);
         assertEquals(TEST_DATETIME, new DateTime(
@@ -40,5 +62,9 @@ public class DateSerializationTest {
 
     public static class PostWithLongDate {
         public long createdAt;
+    }
+
+    public static class PostWithStringDate {
+        public String createdAt;
     }
 }
