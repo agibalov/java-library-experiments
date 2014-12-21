c91b8e77e1869b6ed04cf41abbca6439bde29160
loki2302 <loki2302@loki2302.me>
2014-12-21 11:22:35 +0300

    Extended CustomRunnerTest to check RunCount, FailureCount and IgnoreCount

--- src/test/java/me/loki2302/CustomRunnerTest.java
+++ src/test/java/me/loki2302/CustomRunnerTest.java
@@ -1,7 +1,9 @@
 package me.loki2302;
 
+import org.junit.ComparisonFailure;
 import org.junit.Test;
 import org.junit.runner.*;
+import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunNotifier;
 
 import static org.junit.Assert.assertEquals;
@@ -11,7 +13,17 @@ public class CustomRunnerTest {
     public void canUseCustomRunner() {
         JUnitCore core = new JUnitCore();
         Result result = core.run(DummyTest.class);
-        assertEquals(3, result.getRunCount());
+
+        // it's not quite clear what all these numbers mean
+        // I interpret it like this:
+        //   RunCount - tests which at least have been started (NOT ignored)
+        //   FailureCount - tests which have been started but then failed
+        //   IgnoreCount - tests which have been ignored
+        // So, TotalTests = RunCount + IgnoreCount
+
+        assertEquals(2, result.getRunCount());
+        assertEquals(1, result.getFailureCount());
+        assertEquals(1, result.getIgnoreCount());
     }
 
     @RunWith(DummyRunner.class)
@@ -31,19 +43,44 @@ public class CustomRunnerTest {
 
         @Override
         public Description getDescription() {
-            return Description.EMPTY; // ???
+            return Description.createSuiteDescription(testClass);
         }
 
         @Override
         public void run(RunNotifier notifier) {
-            notifier.fireTestStarted(Description.EMPTY); // ???
-            notifier.fireTestFinished(Description.EMPTY); // ???
+            if(true) {
+                Description description = Description.createTestDescription(
+                        testClass,
+                        "Test that succeeds");
 
-            notifier.fireTestStarted(Description.EMPTY); // ???
-            notifier.fireTestFinished(Description.EMPTY); // ???
+                notifier.fireTestStarted(description);
+                notifier.fireTestFinished(description);
+            }
 
-            notifier.fireTestStarted(Description.EMPTY); // ???
-            notifier.fireTestFinished(Description.EMPTY); // ???
+            if(true) {
+                Description description = Description.createTestDescription(
+                        testClass,
+                        "Test that fails");
+
+                notifier.fireTestStarted(description);
+
+                Failure failure = new Failure(
+                        description,
+                        new ComparisonFailure(
+                                "Some sort of comparison failure",
+                                "loki2302",
+                                "10k12302"));
+                notifier.fireTestFailure(failure);
+                notifier.fireTestFinished(description);
+            }
+
+            if(true) {
+                Description description = Description.createTestDescription(
+                        testClass,
+                        "An ignored test");
+
+                notifier.fireTestIgnored(description);
+            }
         }
     }
 }
