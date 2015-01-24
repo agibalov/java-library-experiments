943e0e4c837f4a9735eacdfae05cc2d3e4e7fee5
loki2302 <loki2302@loki2302.me>
2015-01-24 23:24:23 +0300

    Reimplemented TransactionsTest to make it look like its single-threaded

--- src/test/java/me/loki2302/TransactionsTest.java
+++ src/test/java/me/loki2302/TransactionsTest.java
@@ -3,10 +3,10 @@ package me.loki2302;
 import org.junit.Test;
 
 import java.sql.*;
-import java.util.concurrent.Exchanger;
+import java.util.concurrent.*;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class TransactionsTest {
     private final static String JDBC_URL = "jdbc:hsqldb:mem:test";
@@ -14,10 +14,10 @@ public class TransactionsTest {
     private final static String JDBC_PASSWORD = "";
 
     @Test
-    public void dummy() throws ClassNotFoundException, SQLException, InterruptedException {
+    public void dummy() throws Exception {
         Class.forName("org.hsqldb.jdbcDriver");
 
-        try(Connection connection = makeConnection()) {
+        try(Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD)) {
             try (PreparedStatement s = connection.prepareStatement(
                     "create table Notes(id int not null, text varchar(256) not null)")) {
                 s.executeUpdate();
@@ -28,117 +28,120 @@ public class TransactionsTest {
             System.out.printf("supports serializable: %b\n", supportsSerializable);
         }
 
-        final Exchanger<Void> thread1BeforeInsertExchanger = new Exchanger<>();
-        final Exchanger<Void> thread1BeforeCommitExchanger = new Exchanger<>();
-        final Actor actor1 = new Actor("thread1", thread1BeforeInsertExchanger, thread1BeforeCommitExchanger);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    actor1.act();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
+        ExecutorService executorServiceA = Executors.newSingleThreadExecutor();
+        SyncExecutor syncExecutorA = new SyncExecutor(executorServiceA);
 
-        final Exchanger<Void> thread2BeforeInsertExchanger = new Exchanger<>();
-        final Exchanger<Void> thread2BeforeCommitExchanger = new Exchanger<>();
-        final Actor actor2 = new Actor("thread2", thread2BeforeInsertExchanger, thread2BeforeCommitExchanger);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    actor2.act();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
+        ExecutorService executorServiceB = Executors.newSingleThreadExecutor();
+        SyncExecutor syncExecutorB = new SyncExecutor(executorServiceB);
 
-        thread1BeforeInsertExchanger.exchange(null);
-        thread2BeforeInsertExchanger.exchange(null);
-        thread2BeforeCommitExchanger.exchange(null);
-        thread1BeforeCommitExchanger.exchange(null);
+        Connection connectionA = syncExecutorA.execute(() -> {
+            Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+            connection.setAutoCommit(false);
+            connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+            return connection;
+        });
+        Connection connectionB = syncExecutorB.execute(() -> {
+            Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+            connection.setAutoCommit(false);
+            connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+            return connection;
+        });
 
-        try(Connection connection = makeConnection()) {
-            try(PreparedStatement s = connection.prepareStatement("select * from Notes")) {
-                try(ResultSet resultSet = s.executeQuery()) {
+        syncExecutorA.execute(() -> {
+            try (PreparedStatement s = connectionA.prepareStatement("select * from Notes")) {
+                s.setQueryTimeout(1);
+                try (ResultSet resultSet = s.executeQuery()) {
                     while (resultSet.next()) {
-                        System.out.printf("xxx %d %s\n", resultSet.getInt(1), resultSet.getString(2));
+                        System.out.printf("A: %d %s\n", resultSet.getInt(1), resultSet.getString(2));
                     }
                 }
             }
-        }
+        });
+        syncExecutorB.execute(() -> {
+            try (PreparedStatement s = connectionB.prepareStatement("select * from Notes")) {
+                s.setQueryTimeout(1);
+                try (ResultSet resultSet = s.executeQuery()) {
+                    while (resultSet.next()) {
+                        System.out.printf("B: %d %s\n", resultSet.getInt(1), resultSet.getString(2));
+                    }
+                }
+            }
+        });
 
-        assertTrue(actor1.managedToInsert());
-        assertFalse(actor2.managedToInsert());
-    }
-
-    private static Connection makeConnection() throws SQLException {
-        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
-    }
-
-    private static class Actor {
-        private final String name;
-        private final Exchanger<Void> beforeInsertExchanger;
-        private final Exchanger<Void> beforeCommitExchanger;
-        private boolean managedToInsert;
-
-        public Actor(
-                String name,
-                Exchanger<Void> beforeInsertExchanger,
-                Exchanger<Void> beforeCommitExchanger) {
-
-            this.name = name;
-            this.beforeInsertExchanger = beforeInsertExchanger;
-            this.beforeCommitExchanger = beforeCommitExchanger;
-        }
-
-        public boolean managedToInsert() {
-            return managedToInsert;
-        }
-
-        public void act() throws SQLException, InterruptedException {
-            try(Connection connection = makeConnection()) {
-                connection.setAutoCommit(false);
-                connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
-
-                try (PreparedStatement s = connection.prepareStatement("select * from Notes")) {
+        syncExecutorA.execute(() -> {
+            try (PreparedStatement s = connectionA.prepareStatement("insert into Notes(id, text) values(?, ?)")) {
+                s.setInt(1, 1);
+                s.setString(2, "created by A");
+                s.setQueryTimeout(1);
+                s.executeUpdate();
+            }
+        });
+        try {
+            syncExecutorB.execute(() -> {
+                try (PreparedStatement s = connectionB.prepareStatement("insert into Notes(id, text) values(?, ?)")) {
+                    s.setInt(1, 1);
+                    s.setString(2, "created by B");
                     s.setQueryTimeout(1);
-                    try (ResultSet resultSet = s.executeQuery()) {
+                    s.executeUpdate();
+                }
+            });
+
+            fail("client 2 managed to update the table after client 1 has already done it");
+        } catch(SQLTransactionRollbackException e) {
+            System.out.println("hurray!");
+        } finally {
+            syncExecutorA.execute(() -> connectionA.commit());
+            syncExecutorB.execute(() -> connectionB.commit());
+
+            syncExecutorA.execute(() -> connectionA.close());
+            syncExecutorB.execute(() -> connectionB.close());
+
+            executorServiceA.shutdown();
+            executorServiceB.shutdown();
+
+            int noteCount = 0;
+            try(Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD)) {
+                try(PreparedStatement s = connection.prepareStatement("select * from Notes")) {
+                    try(ResultSet resultSet = s.executeQuery()) {
                         while (resultSet.next()) {
-                            System.out.printf("%s: %d %s\n", name, resultSet.getInt(1), resultSet.getString(2));
+                            System.out.printf("xxx %d %s\n", resultSet.getInt(1), resultSet.getString(2));
+                            ++noteCount;
                         }
                     }
                 }
+            }
 
-                try (PreparedStatement s = connection.prepareStatement("insert into Notes(id, text) values(?, ?)")) {
-                    s.setInt(1, 1);
-                    s.setString(2, "created by " + name);
-                    s.setQueryTimeout(1);
+            assertEquals(1, noteCount);
+        }
+    }
 
-                    beforeInsertExchanger.exchange(null);
+    public static class SyncExecutor {
+        private final ExecutorService executorService;
 
-                    managedToInsert = false;
-                    try {
-                        s.executeUpdate();
-                        managedToInsert = true;
-                    } catch(Exception e) {
-                        managedToInsert = false;
-                    }
-                }
+        public SyncExecutor(ExecutorService executorService) {
+            this.executorService = executorService;
+        }
 
-                beforeCommitExchanger.exchange(null);
+        public void execute(Throwing throwing) throws Exception {
+            execute(() -> {
+                throwing.run();
+                return null;
+            });
+        }
 
-                connection.commit();
-
-                System.out.printf("%s succeeded\n", name);
+        public <TResult> TResult execute(Callable<TResult> callable) throws Exception {
+            Future<TResult> resultFuture = executorService.submit(callable);
+            try {
+                return resultFuture.get();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw (Exception)e.getCause();
             }
         }
+    }
+
+    public static interface Throwing {
+        void run() throws Exception;
     }
 }
