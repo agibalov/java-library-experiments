e89b00a796441bce1befb5a005521b6d9f17932c
loki2302 <loki2302@loki2302.me>
2015-03-01 12:59:20 +0300

    Added canUseSplitter test

--- src/test/java/me/loki2302/StringsTest.java
+++ src/test/java/me/loki2302/StringsTest.java
@@ -1,7 +1,11 @@
 package me.loki2302;
 
 import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,5 +14,14 @@ public class StringsTest {
     public void canUseJoiner() {
         String s = Joiner.on(", ").join("hello", "there", "omg");
         assertEquals("hello, there, omg", s);
+    }
+
+    @Test
+    public void canUseSplitter() {
+        List<String> terms = Lists.newArrayList(Splitter.on(", ").split("hello, there, omg"));
+        assertEquals(3, terms.size());
+        assertEquals("hello", terms.get(0));
+        assertEquals("there", terms.get(1));
+        assertEquals("omg", terms.get(2));
     }
 }
