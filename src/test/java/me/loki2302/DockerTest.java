37ce53aa7e01b659108b974b49c8dd0ae44f3dac
loki2302 <loki2302@loki2302.me>
2015-05-09 17:21:45 +0300

    Added canCreateStartAndStopContainer test

--- src/test/java/me/loki2302/DockerTest.java
+++ src/test/java/me/loki2302/DockerTest.java
@@ -1,15 +1,17 @@
 package me.loki2302;
 
 import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.CreateContainerResponse;
 import com.github.dockerjava.api.model.SearchItem;
 import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.DockerClientConfig;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
+import java.io.InputStream;
 import java.util.List;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class DockerTest {
     @Test
@@ -36,5 +38,37 @@ public class DockerTest {
                 ubuntuSearchItem.isOfficial());
 
         assertNotNull(ubuntuSearchItem);
+    }
+
+    @Test
+    public void canCreateStartAndStopContainer() {
+        DockerClientConfig dockerClientConfig = DockerClientConfig.createDefaultConfigBuilder()
+                .withUri("unix:///var/run/docker.sock")
+                .build();
+        DockerClient dockerClient = DockerClientBuilder.getInstance(dockerClientConfig).build();
+
+        CreateContainerResponse createContainerResponse = dockerClient.createContainerCmd("ubuntu")
+                .withCmd("echo", "hello world")
+                .withCpuset("0")
+                .withAttachStdout(true)
+                .withAttachStderr(true)
+                .exec();
+        try {
+            String containerId = createContainerResponse.getId();
+
+            dockerClient.startContainerCmd(containerId).exec();
+            dockerClient.stopContainerCmd(containerId).exec();
+            Integer result = dockerClient.waitContainerCmd(containerId).exec();
+            assertEquals(0, result.intValue());
+
+            try(InputStream inputStream = dockerClient.logContainerCmd(containerId).withStdOut().exec()) {
+                String output = IOUtils.toString(inputStream);
+                assertTrue(output.contains("hello world"));
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            dockerClient.removeContainerCmd(createContainerResponse.getId());
+        }
     }
 }
