9188fb8cc3392de54c909ed2ac32ac7afdfec059
loki2302 <loki2302@loki2302.me>
2015-05-12 08:34:17 +0300

    Implemented a dummy producer

--- src/main/java/me/loki2302/App.java
+++ src/main/java/me/loki2302/App.java
@@ -18,6 +18,28 @@ import java.util.concurrent.ExecutionException;
 
 public class App {
     public static void main(String[] args) throws InterruptedException, ExecutionException, BrokenBarrierException {
+        String topicName = "the-topic";
+
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092");
+        properties.put("client.id", "DemoProducer");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        Random r = new Random();
+        while(true) {
+            for (int i = 0; i < 10; ++i) {
+                String key = String.format("key-%d", i + 1);
+                String value = String.valueOf(r.nextInt(1000));
+                producer.send(new ProducerRecord<String, String>(topicName, key, value)).get();
+                Thread.sleep(5);
+            }
+            Thread.sleep(300);
+        }
+    }
+
+    private static void kafkaProducerConsumerHelloWorld() throws InterruptedException, ExecutionException, BrokenBarrierException {
         // Topic removal functionality doesn't seem to work reliably, event with "delete.topic.enable" set to true,
         // so I have to create a new topic on each run
         String topicName = "topic-" + UUID.randomUUID().toString();
