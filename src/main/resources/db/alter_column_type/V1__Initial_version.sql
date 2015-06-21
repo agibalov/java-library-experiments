ba43af0d3286f46e0e2470283037cee79ac48b51
loki2302 <loki2302@loki2302.me>
2015-06-21 11:10:53 +0300

    Added 'change column type' test

---
+++ src/main/resources/db/alter_column_type/V1__Initial_version.sql
@@ -0,0 +1,4 @@
+create table table1(
+    id int identity primary key,
+    data varchar(256)
+);
---
+++ src/test/groovy/me/loki2302/AlterColumnTypeTest.groovy
@@ -0,0 +1,34 @@
+package me.loki2302
+
+import groovy.sql.Sql
+import org.flywaydb.core.Flyway
+import org.junit.Test
+
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertNull
+
+class AlterColumnTypeTest extends AbstractFlywayTest {
+    @Test
+    void dummy() {
+        def flyway = new Flyway()
+        flyway.setLocations('db/alter_column_type')
+        flyway.setDataSource(dataSource)
+        flyway.setTarget('1')
+        flyway.migrate()
+
+        Sql sql = new Sql(dataSource)
+        // InformationSchemaUtils.assertColumnDoesNotExist(sql, 'table2', 'data2')
+
+        sql.executeInsert('insert into table1(data) values(?)', ['123'])
+
+        flyway.setTarget('2')
+        flyway.migrate()
+
+        // InformationSchemaUtils.assertColumnExists(sql, 'table1', 'data2')
+
+        def rows = sql.rows('select * from table1')
+        assertEquals(1, rows.size())
+
+        assertEquals(123, rows[0]['data'])
+    }
+}
