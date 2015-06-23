2dc301458018ec0dade1d3077af5f8b431aaaaf0
loki2302 <loki2302@loki2302.me>
2015-06-23 08:37:07 +0300

    Added embedded Redis server

--- src/test/java/me/loki2302/AbstractJedisTest.java
+++ src/test/java/me/loki2302/AbstractJedisTest.java
@@ -1,15 +1,28 @@
 package me.loki2302;
 
+import org.junit.After;
 import org.junit.Before;
 
 import redis.clients.jedis.Jedis;
+import redis.embedded.RedisServer;
+
+import java.io.IOException;
 
 public abstract class AbstractJedisTest {
+    private RedisServer redisServer;
     protected Jedis jedis;
     
     @Before
-    public void setUpJedis() {
-        jedis = new Jedis("192.168.1.99");
+    public void startRedis() throws IOException {
+        redisServer = new RedisServer();
+        redisServer.start();
+
+        jedis = new Jedis("localhost");
         jedis.flushAll();
+    }
+
+    @After
+    public void stopRedis() {
+        redisServer.stop();
     }
 }
