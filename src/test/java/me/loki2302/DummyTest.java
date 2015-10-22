01c83510a7f9d2e0222eddc06168ec95f9da33fa
loki2302 <loki2302@loki2302.me>
2015-10-22 23:52:10 +0300

    Added a draft of programmatic BpmnModel construction test

--- src/test/java/me/loki2302/DummyTest.java
+++ src/test/java/me/loki2302/DummyTest.java
@@ -1,5 +1,7 @@
 package me.loki2302;
 
+import org.activiti.bpmn.model.*;
+import org.activiti.bpmn.model.Process;
 import org.activiti.engine.*;
 import org.activiti.engine.identity.User;
 import org.activiti.engine.repository.Deployment;
@@ -7,6 +9,7 @@ import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -94,5 +97,54 @@ public class DummyTest {
                 .singleResult()
                 .getEndTime());
         //
+    }
+
+    @Test
+    public void xxx() {
+        UserTask userTask = new UserTask();
+        userTask.setId("tid");
+        userTask.setCandidateGroups(Arrays.asList("dummies"));
+
+        Process p = new Process();
+        p.setName("pname");
+        p.setId("pid");
+        p.addFlowElement(new StartEvent() {{
+            setId("start");
+        }}.clone());
+        p.addFlowElement(userTask);
+        p.addFlowElement(new EndEvent() {{setId("end");}}.clone());
+        p.addFlowElement(new SequenceFlow("start", "tid"));
+        p.addFlowElement(new SequenceFlow("tid", "end"));
+
+        BpmnModel m = new BpmnModel();
+        m.addProcess(p);
+
+        ProcessEngine processEngine = ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration()
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_CREATE_DROP)
+                .setJdbcUrl("jdbc:h2:mem:my-own-db2;DB_CLOSE_DELAY=1000")
+                .setAsyncExecutorEnabled(true)
+                .setAsyncExecutorActivate(false)
+                .setJobExecutorActivate(false)
+                .buildProcessEngine();
+
+        IdentityService identityService = processEngine.getIdentityService();
+        identityService.saveGroup(identityService.newGroup("dummies"));
+
+        User dummyUser = identityService.newUser("dummy");
+        dummyUser.setPassword("dummy");
+        identityService.saveUser(dummyUser);
+        identityService.createMembership("dummy", "dummies");
+
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        Deployment deployment = repositoryService.createDeployment()
+                .addBpmnModel("xxx.bpmn", m)
+                .deploy();
+
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("pid");
+
+        assertEquals(1, processEngine.getTaskService()
+                .createTaskQuery()
+                .taskCandidateGroup("dummies").list().size());
     }
 }
