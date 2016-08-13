2034f5f573afe2863534c4941c090e701fb24628
loki2302 <loki2302@loki2302.me>
2016-08-13 10:54:47 -0400

    Added canUseCustomConfigurationFile test

--- src/test/java/me/loki2302/DummyTest.java
+++ src/test/java/me/loki2302/DummyTest.java
@@ -4,17 +4,25 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.joran.JoranConfigurator;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.OutputStreamAppender;
+import ch.qos.logback.core.joran.spi.JoranException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayOutputStream;
 import java.nio.charset.Charset;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class DummyTest {
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
     @Test
     public void canConfigureLoggingAtRuntime() {
         LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
@@ -43,5 +51,21 @@ public class DummyTest {
         assertTrue(loggedString.contains("\n"));
         assertTrue(loggedString.contains("hi there"));
         assertTrue(loggedString.contains("hi omg"));
+    }
+
+    @Test
+    public void canUseCustomConfigurationFile() throws JoranException {
+        org.slf4j.Logger logger = LoggerFactory.getLogger("dummy");
+
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+        JoranConfigurator joranConfigurator = new JoranConfigurator();
+        joranConfigurator.setContext(loggerContext);
+        loggerContext.reset();
+        joranConfigurator.doConfigure(DummyTest.class.getResource("/dummy.xml"));
+
+        logger.info("hello");
+
+        assertTrue(systemOutRule.getLog().contains("INFO hello"));
     }
 }
