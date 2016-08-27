ea7547445867f24f7a68a5282729e33309154821
loki2302 <loki2302@loki2302.me>
2016-08-27 19:30:26 -0400

    Added checkHowItWorksWithJavadoc test

--- src/test/groovy/me/loki2302/QDoxTest.groovy
+++ src/test/groovy/me/loki2302/QDoxTest.groovy
@@ -1,6 +1,7 @@
 package me.loki2302;
 
 import com.thoughtworks.qdox.JavaProjectBuilder
+import com.thoughtworks.qdox.model.DocletTag
 import com.thoughtworks.qdox.model.JavaClass
 import com.thoughtworks.qdox.model.JavaMethod
 import com.thoughtworks.qdox.model.JavaSource
@@ -38,5 +39,41 @@ public class Dummy {
         assertEquals('int', addNumbersMethod.returns.value)
         assertEquals('float', addNumbersMethod.parameters[0].type.value)
         assertEquals('double', addNumbersMethod.parameters[1].type.value)
+    }
+
+    @Test
+    void checkHowItWorksWithJavadoc() {
+        JavaSource javaSource = new JavaProjectBuilder().addSource(new File('src/main/java/me/loki2302/CalculatorService.java'))
+        assertEquals(1, javaSource.classes.size())
+
+        JavaClass calculatorServiceClass = javaSource.classes[0]
+        assertEquals('CalculatorService', calculatorServiceClass.name)
+
+        calculatorServiceClass.getClassNamePrefix()
+
+        if(true) {
+            DocletTag returnDocletTag = calculatorServiceClass.getTagByName('return')
+            assertNull(returnDocletTag) // Sure class doesn't have a return tag
+
+            assertEquals('Implements calculator functionality', calculatorServiceClass.comment)
+        }
+
+        assertEquals(2, calculatorServiceClass.methods.size())
+
+        JavaMethod addNumbersMethod = calculatorServiceClass.methods[0]
+        assertEquals('addNumbers', addNumbersMethod.name)
+
+        if(true) {
+            DocletTag returnDocletTag = addNumbersMethod.getTagByName('return')
+            assertNotNull(returnDocletTag)
+            assertEquals('sum of a and b', returnDocletTag.value)
+        }
+
+        if(true) {
+            List<DocletTag> parameterDocletTags = addNumbersMethod.getTagsByName('param')
+            assertEquals(2, parameterDocletTags.size())
+
+            assertEquals(['a', 'number', 'A'], parameterDocletTags[0].parameters)
+        }
     }
 }
