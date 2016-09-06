eb11ac8fa956eaf9d044865a6684451b959f15ef
loki2302 <loki2302@loki2302.me>
2016-09-05 23:24:28 -0400

    Added canDetectCycles test

--- src/test/java/me/loki2302/AppTest.java
+++ src/test/java/me/loki2302/AppTest.java
@@ -1,6 +1,7 @@
 package me.loki2302;
 
 import org.jgrapht.DirectedGraph;
+import org.jgrapht.alg.CycleDetector;
 import org.jgrapht.ext.DOTImporter;
 import org.jgrapht.ext.ImportException;
 import org.jgrapht.graph.AbstractBaseGraph;
@@ -8,8 +9,6 @@ import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
@@ -18,19 +17,12 @@ import static org.junit.Assert.assertTrue;
 public class AppTest {
     @Test
     public void canGetAllVerticesAndEdges() throws ImportException {
-        DirectedGraph<String, DefaultEdge> g
-                = new DefaultDirectedGraph<>(DefaultEdge.class);
-
-        DOTImporter<String, DefaultEdge> importer = new DOTImporter<>(
-                (label, attributes) -> label,
-                (from, to, label, attributes) -> new DefaultEdge());
-
-        importer.read(String.join("\n",
+        DirectedGraph<String, DefaultEdge> g = graphFromDOTLines(
                 "digraph G {",
                 " a -> b;",
                 " b -> c;",
                 " c -> a;",
-                "}"), (AbstractBaseGraph<String, DefaultEdge>)g);
+                "}");
 
         Set<String> vertexSet = g.vertexSet();
         assertEquals(3, vertexSet.size());
@@ -43,5 +35,31 @@ public class AppTest {
         assertTrue(edgeSet.stream().anyMatch(e -> g.getEdgeSource(e).equals("a") && g.getEdgeTarget(e).equals("b")));
         assertTrue(edgeSet.stream().anyMatch(e -> g.getEdgeSource(e).equals("b") && g.getEdgeTarget(e).equals("c")));
         assertTrue(edgeSet.stream().anyMatch(e -> g.getEdgeSource(e).equals("c") && g.getEdgeTarget(e).equals("a")));
+    }
+
+    @Test
+    public void canDetectCycles() throws ImportException {
+        DirectedGraph<String, DefaultEdge> g = graphFromDOTLines(
+                "digraph G {",
+                " a -> b;",
+                " b -> c;",
+                " c -> a;",
+                "}");
+
+        CycleDetector<String, DefaultEdge> cycleDetector = new CycleDetector<>(g);
+        assertTrue(cycleDetector.detectCycles());
+    }
+
+    private static DirectedGraph<String, DefaultEdge> graphFromDOTLines(String... lines) throws ImportException {
+        DirectedGraph<String, DefaultEdge> g
+                = new DefaultDirectedGraph<>(DefaultEdge.class);
+
+        DOTImporter<String, DefaultEdge> importer = new DOTImporter<>(
+                (label, attributes) -> label,
+                (from, to, label, attributes) -> new DefaultEdge());
+
+        importer.read(String.join("\n", lines), (AbstractBaseGraph<String, DefaultEdge>)g);
+
+        return g;
     }
 }
