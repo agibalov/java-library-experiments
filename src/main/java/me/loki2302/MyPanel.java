5f5bbc234e6e399b83b43c85816c749aec2e4190
loki2302 <loki2302@loki2302.me>
2016-11-07 23:07:53 -0500

    Managed to bind UI element to panel object property

--- src/main/java/me/loki2302/MyPanel.java
+++ src/main/java/me/loki2302/MyPanel.java
@@ -1,14 +1,28 @@
 package me.loki2302;
 
 import com.vaadin.annotations.DesignRoot;
-import com.vaadin.ui.Panel;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.declarative.Design;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @DesignRoot
-public class MyPanel extends Panel {
-    // How do I connect this to UI component?
-    // What's the view and what's the UI?
+public class MyPanel extends HorizontalLayout {
+    private final static Logger LOGGER = LoggerFactory.getLogger(MyPanel.class);
+
+    Button theButton;
+
     public MyPanel() {
-        Design.read("MyPanel.html", this);
+        Design.read(this);
+
+        theButton.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                LOGGER.info("Omg!");
+                Notification.show("MyPanel says hi!");
+            }
+        });
     }
 }
