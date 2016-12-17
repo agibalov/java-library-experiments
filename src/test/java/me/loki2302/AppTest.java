c6d2eb63aee2e7a6946abfa8c766252b228b8924
loki2302 <loki2302@loki2302.me>
2016-12-17 14:27:52 -0500

    Added renderToPng() and renderToSvg()

--- src/test/java/me/loki2302/AppTest.java
+++ src/test/java/me/loki2302/AppTest.java
@@ -1,10 +1,22 @@
 package me.loki2302;
 
-import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.batik.dom.GenericDOMImplementation;
+import org.apache.batik.ext.awt.image.spi.ImageWriter;
+import org.apache.batik.ext.awt.image.spi.ImageWriterRegistry;
+import org.apache.batik.svggen.DefaultExtensionHandler;
+import org.apache.batik.svggen.ImageHandlerBase64Encoder;
+import org.apache.batik.svggen.ImageHandlerPNGEncoder;
+import org.apache.batik.svggen.SVGGraphics2D;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.junit.Test;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -47,5 +59,4 @@ public class AppTest {
         assertTrue(text.contains("This is a title"));
         assertTrue(text.contains("And this is some dummy text."));
     }
-
 }
