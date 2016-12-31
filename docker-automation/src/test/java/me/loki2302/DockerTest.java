306ac5193a89d8a2aa9a6c46619295fcb62e84ad
loki2302 <loki2302@loki2302.me>
2016-12-30 21:40:09 -0500

    Added Docker networking test

--- docker-automation/src/test/java/me/loki2302/DockerTest.java
+++ docker-automation/src/test/java/me/loki2302/DockerTest.java
@@ -2,12 +2,12 @@ package me.loki2302;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.CreateContainerResponse;
-import com.github.dockerjava.api.model.Frame;
-import com.github.dockerjava.api.model.SearchItem;
-import com.github.dockerjava.api.model.StreamType;
+import com.github.dockerjava.api.exception.NotModifiedException;
+import com.github.dockerjava.api.model.*;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.RemoteApiVersion;
 import com.github.dockerjava.core.command.LogContainerResultCallback;
 import com.github.dockerjava.core.command.PullImageResultCallback;
 import com.github.dockerjava.core.command.WaitContainerResultCallback;
@@ -24,7 +24,6 @@ public class DockerTest {
     @Test
     public void canFindUbuntu() {
         DockerClientConfig dockerClientConfig = DefaultDockerClientConfig.createDefaultConfigBuilder()
-                .withApiVersion("1.23")
                 .build();
         DockerClient dockerClient = DockerClientBuilder.getInstance(dockerClientConfig).build();
 
@@ -83,6 +82,94 @@ public class DockerTest {
             assertEquals("hello world\n", stdoutFrames.get(0));
         } finally {
             dockerClient.removeContainerCmd(createContainerResponse.getId()).exec();
+        }
+    }
+
+    @Test
+    public void canAttachContainersToANetwork() throws InterruptedException {
+        DockerClientConfig dockerClientConfig = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .build();
+        DockerClient dockerClient = DockerClientBuilder.getInstance(dockerClientConfig).build();
+
+        Network.Ipam.Config networkIpamConfig = new Network.Ipam.Config()
+                .withSubnet("10.100.102.0/24");
+        Network.Ipam networkIpam = new Network.Ipam().withConfig(networkIpamConfig);
+        String networkId = dockerClient.createNetworkCmd()
+                .withName("dummyNetwork")
+                .withIpam(networkIpam)
+                .exec()
+                .getId();
+
+        System.out.printf("dummyNetwork network has id %s\n", networkId);
+
+        try {
+            List<Network> networks = dockerClient.listNetworksCmd().exec();
+            for (Network network : networks) {
+                System.out.printf("id=%s, name=%s\n", network.getId(), network.getName());
+            }
+
+            dockerClient.pullImageCmd("ubuntu:14.04")
+                    .exec(new PullImageResultCallback())
+                    .awaitSuccess();
+
+            String containerId = dockerClient.createContainerCmd("ubuntu:14.04")
+                    .withCmd("ifconfig")
+                    .exec()
+                    .getId();
+            try {
+                ContainerNetwork.Ipam containerNetworkIpam = new ContainerNetwork.Ipam()
+                        .withIpv4Address("10.100.102.11");
+                ContainerNetwork containerNetwork = new ContainerNetwork()
+                        .withNetworkID(networkId)
+                        .withIpamConfig(containerNetworkIpam);
+                dockerClient.connectToNetworkCmd()
+                        .withContainerId(containerId)
+                        .withNetworkId(networkId)
+                        .withContainerNetwork(containerNetwork)
+                        .exec();
+
+                dockerClient.startContainerCmd(containerId).exec();
+
+                Network updatedNetwork = dockerClient.inspectNetworkCmd().withNetworkId(networkId).exec();
+                System.out.println(updatedNetwork.getContainers().entrySet().stream()
+                        .map(e -> String.format("%s: %s", e.getKey(), e.getValue().getIpv4Address()))
+                        .collect(Collectors.joining("\n")));
+
+                try {
+                    dockerClient.stopContainerCmd(containerId).exec();
+                } catch (NotModifiedException e) {
+                    // https://github.com/docker-java/docker-java/issues/98
+                    // This exception is OK. It just means that container had already been stopped when I made this call.
+                }
+
+                int result = dockerClient.waitContainerCmd(containerId)
+                        .exec(new WaitContainerResultCallback())
+                        .awaitStatusCode();
+                assertEquals(0, result);
+
+                List<Frame> frames = new ArrayList<>();
+                dockerClient.logContainerCmd(containerId)
+                        .withStdOut(true)
+                        .exec(new LogContainerResultCallback() {
+                            @Override
+                            public void onNext(Frame frame) {
+                                frames.add(frame);
+                            }
+                        })
+                        .awaitCompletion();
+
+                List<String> stdoutFrames = frames.stream()
+                        .filter(f -> f.getStreamType().equals(StreamType.STDOUT))
+                        .map(f -> new String(f.getPayload(), Charset.forName("UTF-8")))
+                        .collect(Collectors.toList());
+                System.out.println(stdoutFrames.stream().collect(Collectors.joining()));
+
+                assertTrue(stdoutFrames.stream().anyMatch(f -> f.contains("inet addr:10.100.102.11")));
+            } finally {
+                dockerClient.removeContainerCmd(containerId).exec();
+            }
+        } finally {
+            dockerClient.removeNetworkCmd("dummyNetwork").exec();
         }
     }
 }
