291142bfc9645306b402166b310818e417ca8dd4
loki2302 <loki2302@loki2302.me>
2017-09-14 21:15:17 -0400

    Added canDeserializeDayOfWeekAndDuration

--- src/test/java/me/loki2302/serializer/Java8DateTimeSerializationTest.java
+++ src/test/java/me/loki2302/serializer/Java8DateTimeSerializationTest.java
@@ -5,9 +5,13 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.time.DayOfWeek;
+import java.time.Duration;
 import java.time.Instant;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class Java8DateTimeSerializationTest {
     private final static ObjectMapper OBJECT_MAPPER = new ObjectMapper()
@@ -15,17 +19,37 @@ public class Java8DateTimeSerializationTest {
 
     @Test
     public void canDeserializeSecondsToInstant() throws IOException {
-        SomeEvent someEvent = OBJECT_MAPPER.readValue("{\"timestamp\": 0}", SomeEvent.class);
+        SomeEvent someEvent = OBJECT_MAPPER.readValue(
+                "{\"timestamp\": 0}",
+                SomeEvent.class);
         assertEquals("1970-01-01T00:00:00Z", someEvent.timestamp.toString());
     }
 
     @Test
     public void canDeserializeIso8601ZToInstant() throws IOException {
-        SomeEvent someEvent = OBJECT_MAPPER.readValue("{\"timestamp\": \"1970-01-01T00:00:00Z\"}", SomeEvent.class);
+        SomeEvent someEvent = OBJECT_MAPPER.readValue(
+                "{\"timestamp\": \"1970-01-01T00:00:00Z\"}",
+                SomeEvent.class);
         assertEquals(0, someEvent.timestamp.getEpochSecond());
+    }
+
+    @Test
+    public void canDeserializeDayOfWeekAndDuration() throws IOException {
+        SomeConfig someConfig = OBJECT_MAPPER.readValue(
+                "{\"daysOfWeek\":[\"MONDAY\", \"WEDNESDAY\"], \"duration\": \"PT5M\"}",
+                SomeConfig.class);
+        assertEquals(2, someConfig.daysOfWeek.size());
+        assertTrue(someConfig.daysOfWeek.contains(DayOfWeek.MONDAY));
+        assertTrue(someConfig.daysOfWeek.contains(DayOfWeek.WEDNESDAY));
+        assertEquals(Duration.ofMinutes(5), someConfig.duration);
     }
 
     public static class SomeEvent {
         public Instant timestamp;
+    }
+
+    public static class SomeConfig {
+        public Set<DayOfWeek> daysOfWeek;
+        public Duration duration;
     }
 }
