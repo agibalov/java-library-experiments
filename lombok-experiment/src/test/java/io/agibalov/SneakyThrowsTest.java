656da55d1c176f72042481616476cb68f618ecec
agibalov <loki2302@loki2302.me>
2018-02-08 18:48:58 -0500

    Added SneakyThrowsTest

---
+++ lombok-experiment/src/test/java/io/agibalov/SneakyThrowsTest.java
@@ -0,0 +1,18 @@
+package io.agibalov;
+
+import lombok.SneakyThrows;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class SneakyThrowsTest {
+    @Test(expected = IOException.class)
+    public void xxx() {
+        someMethodThatThrowsIoException();
+    }
+
+    @SneakyThrows
+    private static void someMethodThatThrowsIoException() {
+        throw new IOException("I am IOException!");
+    }
+}
