36359b66a61182b5421da06abdc7650cca2ac6c3
agibalov <loki2302@loki2302.me>
2018-07-21 18:43:33 -0400

    Fixed the README

--- jooq-experiment/README.md
+++ jooq-experiment/README.md
@@ -1,5 +1,5 @@
 # jooq-experiment
 
-1. `./gradlew flywayMigrate` to generate a dummy H2 database at `./db`
+1. `./gradlew flywayMigrate` to generate a dummy H2 database at `./dbsample`
 2. `./gradlew generateDummyJooqSchemaSource` to generate jOOQ bindings based on previously generated dummy database.
 3. `./gradlew test` to run tests.
--- jooq-experiment/src/test/java/me/loki2302/CRUDTest.java
+++ jooq-experiment/src/test/java/me/loki2302/CRUDTest.java
@@ -9,10 +9,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
