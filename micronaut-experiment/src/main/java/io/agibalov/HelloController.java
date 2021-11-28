48962f7c01fb93571cb2e9f43255176a639caaec
agibalov <andrey.agibalov@gmail.com>
2021-11-28 15:06:15 -0500

    LABS-1485: save progress

--- micronaut-experiment/src/main/java/io/agibalov/HelloController.java
+++ micronaut-experiment/src/main/java/io/agibalov/HelloController.java
@@ -5,7 +5,9 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class HelloController {
     @Inject
@@ -16,6 +18,7 @@ public class HelloController {
 
     @Get(produces = MediaType.TEXT_PLAIN)
     public String index() {
+        log.info("index() called!");
         return String.format("this is the index page. suffix: %s", helloMessageSuffix);
     }
 
